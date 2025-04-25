a6140e412906606ae94744c8ec9aa59a5ad33fd8
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-25 11:23:35 +0200

    added: animations

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-show="posts.length > 0">
     <h3>List of posts</h3>
-    <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list" tag="div">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-show="posts.length === 0" style="color: red">The list is empty</h2>
 </template>
@@ -28,4 +30,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
